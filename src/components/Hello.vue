c40d60a8468a2e91057fc5bbd7002e1263278c1c
Justin Lin <justinwlin@gmail.com>
2022-09-15 22:23:54 -0400

    Setting up basic repo

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,10 +1,33 @@
 <script lang="ts" setup>
-defineProps<{ msg: string }>()
+  import axios from "axios";
+  import { reactive } from "vue";
+  const props = defineProps<{ msg: string }>();
+
+  const data = reactive({
+    jsonPlaceHolder: null,
+  });
+  function handleClick() {
+    console.log("clicked");
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => {
+        console.log(response);
+        data.jsonPlaceHolder = response.data;
+        console.log(data.jsonPlaceHolder);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 </script>
 
 <template>
   <h3>{{ msg }}</h3>
-
+  <button @click="handleClick">{{ msg }}</button>
+  <p>
+    Hello
+    {{ data.jsonPlaceHolder }}
+  </p>
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
@@ -18,27 +41,28 @@ defineProps<{ msg: string }>()
   </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"
+      >Vite Docs</a
+    >
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
 </style>
-
